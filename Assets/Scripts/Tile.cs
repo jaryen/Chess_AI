1ce87f2a0f4c108c17d146bd21b453b30556c005
Jared Yen <jaryen@uw.edu>
2021-09-01 01:05:20 -0700

    Updated Tile Class

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    private SpriteRenderer tile;
+    private Color tileCol;
+    private Piece currentPiece;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tile = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+        currentPiece = null;
+    }
+
+    // Set color of tile
+    public void SetColor(Color c)
+    {
+        tileCol = c;
+        SpriteRenderer sr = tile.GetComponent<SpriteRenderer>();
+        sr.color = tileCol;
+    }
+
+    public Color GetColor()
+    {
+        return tileCol;
+    }
+
+    // Set the current piece of this tile
+    public void SetCurrentPiece(Piece piece)
+    {
+        currentPiece = piece;
+    }
+
+    // When tile is clicked, set whatever is
+    // in currentPiece to the selectedPiece
+    public Piece GetCurrentPiece()
+    {
+        return currentPiece;
     }
 
     // Update is called once per frame
