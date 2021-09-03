2d0e999bd6a252f348b0749773a4c59b4ab2d9fe
brentli17 <44733139+brentli17@users.noreply.github.com>
2021-09-03 15:45:44 -0700

    knight findMoves() implemented

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Knight : Piece
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,69 @@ public class Knight : Piece
     //finds all possible moves
     public void findMoves()
     {
+        if (this.row <= 5) { //knight can move up
+            //check top left
+            if (gameBoard[this.row + 2, this.col - 1].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row + 2, this.col - 1].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row + 2, this.col - 1]);
+            }
 
+            //check top right
+            if (gameBoard[this.row + 2, this.col + 1].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row + 2, this.col + 1].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row + 2, this.col + 1]);
+            }
+        }
+
+        if (this.row >= 2) { //knight can move down
+            //check bottom left
+            if (gameBoard[this.row - 2, this.col - 1].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row - 2, this.col - 1].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row - 2, this.col - 1]);
+            }
+
+            //check bottom right
+            if (gameBoard[this.row - 2, this.col + 1].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row - 2, this.col + 1].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row - 2, this.col + 1]);
+            }
+        }
+
+        if (this.col <= 5) { //knight can move right
+            //check right top
+            if (gameBoard[this.row + 1, this.col + 2].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row + 1, this.col + 2].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row + 1, this.col + 2]);
+            }
+
+            //check right bottom
+            if (gameBoard[this.row - 1, this.col + 2].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row - 1, this.col + 2].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row - 1, this.col + 2]);
+            }
+        }
+
+        if (this.col >= 2) { //knight can move left
+            //check left top
+            if (gameBoard[this.row + 1, this.col - 2].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row + 1, this.col - 2].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row + 1, this.col - 2]);
+            }
+
+            //check left bottom
+            if (gameBoard[this.row - 1, this.col - 2].GetComponent<Tile>().GetCurrentPiece() == null ||
+                gameBoard[this.row - 1, this.col - 2].GetComponent<Tile>().GetCurrentPiece().isWhite != this.isWhite)
+            {
+                validMoves.Add(gameBoard[this.row - 1, this.col - 2]);
+            }
+        }       
     }
 
     // Update is called once per frame
